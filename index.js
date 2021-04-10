e831c4cbfb475995849d17cd7504e08e7dad70ab
bakhtiarabid <abid.bu16@gmail.com>
2021-04-11 00:57:42 +0600

    Drum Kit

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-var numberofDrumButtons = document.querySelectorAll(".drum").length;
+//var numberofDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i =0; i<numberofDrumButtons; i++){
-document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+// for (var i =0; i<numberofDrumButtons; i++)
+document.querySelectorAll("button")[0].addEventListener("click", function () {
    alert("I got clicked");
 });
 
-}
+document.querySelectorAll("button")[1].addEventListener("click", function () {
+   alert("I got clicked");
+});
+
+document.querySelectorAll("button")[2].addEventListener("click", function () {
+   alert("I got clicked");
+});
+
+document.querySelectorAll("button")[3].addEventListener("click", function () {
+   alert("I got clicked");
+});
+
+document.querySelectorAll("button")[4].addEventListener("click", function () {
+   alert("I got clicked");
+});
+
 
 
