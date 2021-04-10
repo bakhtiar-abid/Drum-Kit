3ebd85ba174327f734a63dcff3a5b73839331ed0
bakhtiarabid <abid.bu16@gmail.com>
2021-04-11 00:53:12 +0600

    Drum Kit

---
+++ index.js
@@ -0,0 +1,10 @@
+var numberofDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i =0; i<numberofDrumButtons; i++){
+document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+   alert("I got clicked");
+});
+
+}
+
+
